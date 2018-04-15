4306dbe6244dc92e219591cb262446b8516fd658
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-15 23:18:00 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   def create
     student = Student.new(:first_name => params["first_name"], :last_name => params["last_name"])
     if !!student.save
-      redirect_to student_path
+      redirect_to student_path(student)
     end
   end
 
